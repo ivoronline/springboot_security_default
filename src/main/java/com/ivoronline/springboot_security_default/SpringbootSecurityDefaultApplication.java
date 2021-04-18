7588f15b2d94b6e0ee7406939a9189bcd6a1180c
igoreta <ivoronline@gmail.com>
2021-04-18 09:13:33 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_default/SpringbootSecurityDefaultApplication.java
+++ src/main/java/com/ivoronline/springboot_security_default/SpringbootSecurityDefaultApplication.java
@@ -2,9 +2,7 @@ package com.ivoronline.springboot_security_default;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@EnableWebSecurity
 @SpringBootApplication
 public class SpringbootSecurityDefaultApplication {
 
